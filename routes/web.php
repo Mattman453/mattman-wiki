351a7a02aa30d9e82e5641cf0021a294a8886069
Mattman453 <mattjcruickshank@gmail.com>
2026-01-07 15:09:11 -0500

    Added a health check

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'App\Http\Controllers\MainController@homeView');
+Route::get('/health-token', function() {
+    Log::debug('health check');
+    return response()->json([
+        'message' => 'OK',
+    ], 200);
+});
+
+Route::get('/', 'App\Http\Controllers\MainController@homeView')->name('home');
 Route::get('/about', 'App\Http\Controllers\MainController@aboutView');
+Route::get('/login', 'App\Http\Controllers\AuthController@getLoginView');
 Route::get('/game/{game}', 'App\Http\Controllers\GamePageController@showGamePage');
