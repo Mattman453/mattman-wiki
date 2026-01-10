9e0aa7d4fa30028764e4d95649eea9e1707cd742
Mattman453 <mattjcruickshank@gmail.com>
2026-01-09 22:33:42 -0500

    Moved away from docker

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-docker compose build app
-docker compose up -d
-docker compose exec app "composer install"
-docker compose exec app "yarn install"
-docker compose exec app "php artisan key:generate"
-docker compose exec app "yarn build"
