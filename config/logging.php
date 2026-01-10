3d2469b7292200a1cae53d3dd4e2c6089d59c33b
root <root@laravel-server.us-central1-a.c.mattman-wiki-site.internal>
2026-01-10 03:09:32 +0000

    updated logging

--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => 'stack',
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
 
         'stack' => [
             'driver' => 'stack',
-            'channels' => explode(',', (string) env('LOG_STACK', 'single')),
+            'channels' => ['stderr', 'single'],
             'ignore_exceptions' => false,
         ],
 
