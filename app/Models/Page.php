d1cc9b20d5a4321bcae6f0c6489dd7f9b7fb06fd
Mattman453 <mattjcruickshank@gmail.com>
2026-01-16 11:38:17 -0500

    Updated styling for pages with regards to how the body works

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -3,14 +3,43 @@
 namespace App\Models;
 
 use MongoDB\Laravel\Eloquent\Model;
+use stdClass;
 
 class Page extends Model
 {
+    protected $fillable = [
+        'sections',
+        'game',
+        'subtitle',
+        'page',
+    ];
+
     public function user() {
         return $this->belongsTo(User::class);
     }
 
     public function game() {
         return $this->belongsTo(Game::class, 'game', 'game');
+    }
+
+    public static function convertPageBodiesToTypeStyle() {
+        $pages = self::get()->toarray();
+        foreach ($pages as &$page) {
+            foreach ($page['sections'] as $i => &$section) {
+                foreach ($section['body'] as $body) {
+                    if (isset($body['type'])) {
+                        break;
+                    }
+                    $newBody = new stdClass();
+                    $newBody->type = 'text';
+                    $newBody->body = $body;
+                    $section['body'] = $newBody;
+                }
+            }
+            self::updateOrCreate(
+                ['game' => $page['game'], 'subtitle' => $page['subtitle'], 'page' => $page['page']],
+                ['sections' => $page['sections']],
+            );
+        }
     }
 }
