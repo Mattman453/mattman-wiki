abb5e9c83b6cf91cbf9301ab8e6e58a5ce64a07c
Mattman453 <mattjcruickshank@gmail.com>
2026-01-19 16:26:27 -0500

    Updated Infopage to work with new style of pages

--- resources/js/Pages/InfoPage.svelte
+++ resources/js/Pages/InfoPage.svelte
@@ -1,22 +1,15 @@
 <script>
-    import { onMount } from "svelte";
     import Layout from "../Components/Layout.svelte";
 
     let { gameInfo, page, ...otherProps } = $props();
-
-    let isDropdownOpen = $state([]);
-
-    onMount(() => {
-        if (gameInfo.headings) {
-            for (let i = 0; i < gameInfo.headings.length; i++) {
-                isDropdownOpen[gameInfo.headings[i].title] = false;
-            }
-        }
-    });
 </script>
 
 <Layout {gameInfo} {...otherProps}>
     <div class="flex column align-items-center">
+        <div class="title-1" style="margin-top: 0;">
+            {page.page ?? page.subtitle ?? page.game}
+        </div>
+        <hr>
         <div class="flex column" style="max-width: 1200px;">
             {#each page.sections as section (section.title)}
                 {#if section.title != page.sections[0].title}
@@ -26,11 +19,11 @@
                     <div class="title-1">
                         {section.title}
                     </div>
-                    {#each section.body as body}
+                    {#if section.body.type == "text"}
                         <div class="title-6">
-                            {body}
+                            {section.body.data}
                         </div>
-                    {/each}
+                    {/if}
                 </div>
             {/each}
         </div>
@@ -45,6 +38,7 @@
     .title-6 {
         margin: 1em 2em;
         font-size: 16px;
+        white-space: pre-line;
     }
 
     hr {
