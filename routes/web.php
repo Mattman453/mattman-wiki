06c28c6f99d32ff1cc24493d9aad495fa09e985d
Mattman453 <mattjcruickshank@gmail.com>
2026-01-21 05:53:36 -0500

    Switched to using built-in health check routes /up

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/health-token', function() {
-    return response()->json([
-        'message' => 'OK',
-    ], 200);
-});
-
 Route::get('/', 'App\Http\Controllers\MainController@homeView')->name('game.home');
 Route::get('/about', 'App\Http\Controllers\MainController@aboutView');
 Route::get('/login', 'App\Http\Controllers\AuthController@getLoginView')->name('auth.login');
