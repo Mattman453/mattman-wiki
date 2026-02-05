42005b73a08226522a18d0591577fea2490d891b
Mattman453 <mattjcruickshank@gmail.com>
2026-02-05 12:37:25 -0500

    Added global rate limiting to avoid overload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        RateLimiter::for('global', function (Request $request) {
+            if ($request->user()) {
+                if (isset($request->user()->roles) && (in_array('admin', $request->user()->roles) || in_array('author', $request->user()->roles))) return Limit::none();
+                return Limit::perMinute(300)->by($request->user()->id());
+            }
+            return Limit::perMinute(60)->by($request->ip());
+        });
+
         Gate::before(function (User $user, string $ability) {
             if (!isset($user->roles)) return false;
             return in_array('admin', $user->roles);
--- routes/web.php
+++ routes/web.php
@@ -2,39 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Static Pages
-Route::get('/', 'App\Http\Controllers\MainController@homeView')->name('game.home');
-Route::get('/about', 'App\Http\Controllers\MainController@aboutView');
+Route::middleware(['throttle:global'])->group(function () {
+    // Static Pages
+    Route::get('/', 'App\Http\Controllers\MainController@homeView')->name('game.home');
+    Route::get('/about', 'App\Http\Controllers\MainController@aboutView');
+    
+    // Authentication
+    Route::prefix('/login')->group(function () {
+        Route::get('/', 'App\Http\Controllers\AuthController@getLoginView')->name('auth.login');
+        Route::post('/', 'App\Http\Controllers\AuthController@login');
+    });
+    Route::post('/register', 'App\Http\Controllers\AuthController@register');
+    Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
+    Route::prefix('/verify')->group(function () {
+        Route::get('/email/{id}/{hash}', 'App\Http\Controllers\AuthController@verifyEmail')->middleware('signed')->name('verification.verify');
+        Route::get('/', 'App\Http\Controllers\AuthController@getVerifyEmailView')->middleware('auth')->name('verification.notice');
+    });
+    Route::post('/send-verification', 'App\Http\Controllers\AuthController@sendEmailVerification')->middleware('auth')->name('verification.send');
+    
+    // Standard Pages
+    Route::prefix('/game')->group(function () {
+        Route::post('/new_page', 'App\Http\Controllers\PageController@createPage')->middleware(['verified']);
+        Route::post('/edit/{game}/{subtitle?}/{page?}', 'App\Http\Controllers\PageController@updatePage')->middleware(['auth', 'verified']);
+        Route::get('/{game}/{subtitle?}/{page?}', 'App\Http\Controllers\PageController@showStandardPage');
+    });
+    
+    // Generic Post Requests
+    Route::post('/update-lifetime', 'App\Http\Controllers\MainController@updateLifetime');
 
-// Authentication
-Route::prefix('/login')->group(function () {
-    Route::get('/', 'App\Http\Controllers\AuthController@getLoginView')->name('auth.login');
-    Route::post('/', 'App\Http\Controllers\AuthController@login');
-});
-Route::post('/register', 'App\Http\Controllers\AuthController@register');
-Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
-Route::prefix('/verify')->group(function () {
-    Route::get('/email/{id}/{hash}', 'App\Http\Controllers\AuthController@verifyEmail')->middleware('signed')->name('verification.verify');
-    Route::get('/', 'App\Http\Controllers\AuthController@getVerifyEmailView')->middleware('auth')->name('verification.notice');
-});
-Route::post('/send-verification', 'App\Http\Controllers\AuthController@sendEmailVerification')->middleware('auth')->name('verification.send');
-
-// Standard Pages
-Route::prefix('/game')->group(function () {
-    Route::post('/new_page', 'App\Http\Controllers\PageController@createPage')->middleware(['verified']);
-    Route::post('/edit/{game}/{subtitle?}/{page?}', 'App\Http\Controllers\PageController@updatePage')->middleware(['auth', 'verified']);
-    Route::get('/{game}/{subtitle?}/{page?}', 'App\Http\Controllers\PageController@showStandardPage');
-});
-
-// Generic Post Requests
-Route::post('/update-lifetime', 'App\Http\Controllers\MainController@updateLifetime');
-
-// No Page Found
-Route::fallback(function () {
-    $requestUri = request()->path();
-    if (str_ends_with($requestUri, '.php')) {
-        $newUri = substr($requestUri, 0, -4);
-        return redirect($newUri);
-    }
-    return redirect(route('game.home'));
+    // No Page Found
+    Route::fallback(function () {
+        $requestUri = request()->path();
+        if (str_ends_with($requestUri, '.php')) {
+            $newUri = substr($requestUri, 0, -4);
+            return redirect($newUri);
+        }
+        return redirect(route('game.home'));
+    });
 });
