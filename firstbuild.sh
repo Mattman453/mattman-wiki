39905bdeb6d6baa881a735619a51e565c5cd2b55
Mattman453 <mattjcruickshank@gmail.com>
2026-03-20 21:08:43 -0400

    Moved to using the lighter weight php8.5-fpm-alpine

---
+++ firstbuild.sh
@@ -0,0 +1,6 @@
+git pull --rebase
+docker compose build
+docker compose up -d
+docker compose exec app composer install --optimize-autoloader --no-dev
+docker compose exec app php artisan key:generate
+docker compose exec app sh deploy.sh
